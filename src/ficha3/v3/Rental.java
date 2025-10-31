fa5f5ddf56c965b7b6aa395f961007083b953eaa
LETI-111204 <gllca@iscte-iul.pt>
2025-10-31 12:19:36 +0000

    added v3

---
+++ src/ficha3/v3/Rental.java
@@ -0,0 +1,46 @@
+package ficha3.v3;
+
+public class Rental
+{
+	private Movie _movie;
+	private int		_daysRented;
+
+	public Rental(Movie movie, int daysRented)
+	{
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+
+	public int getDaysRented()
+	{
+		return _daysRented;
+	}
+
+	public Movie getMovie()
+	{
+		return _movie;
+	}
+
+    double getAmount() {
+        double thisAmount = 0;
+
+        // determine amounts for each line
+        switch (getMovie().getPriceCode())
+        {
+            case REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2)
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3)
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+}
